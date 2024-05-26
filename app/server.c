b04fe57810251434c0e896f0b27ab5ad555f66c2
Iulian Ciubotariu <129002665+JustIuli@users.noreply.github.com>
2024-05-27 02:47:28 +0300

    pass 1st stage

--- app/server.c
+++ app/server.c
@@ -1,20 +1,60 @@
-#include <errno.h>
-#include <netinet/in.h>
-#include <netinet/ip.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 #include <sys/socket.h>
+#include <netinet/in.h>
+#include <netinet/ip.h>
+#include <string.h>
+#include <errno.h>
 #include <unistd.h>
 int main() {
     // Disable output buffering
     setbuf(stdout, NULL);
-    // You can use print statements as follows for debugging, they'll be visible
-    // when running tests.
+    // You can use print statements as follows for debugging, they'll be visible when running tests.
     printf("Logs from your program will appear here!\n");
-    // Uncomment this block to pass the first stage
     int server_fd, client_addr_len;
     struct sockaddr_in client_addr;
+    // Uncomment this block to pass the first stage
+    //
+    // int server_fd, client_addr_len;
+    // struct sockaddr_in client_addr;
+    //
+    // server_fd = socket(AF_INET, SOCK_STREAM, 0);
+    // if (server_fd == -1) {
+    // 	printf("Socket creation failed: %s...\n", strerror(errno));
+    // 	return 1;
+    // }
+    //
+    // // Since the tester restarts your program quite often, setting REUSE_PORT
+    // // ensures that we don't run into 'Address already in use' errors
+    // int reuse = 1;
+    // if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEPORT, &reuse, sizeof(reuse)) < 0) {
+    // 	printf("SO_REUSEPORT failed: %s \n", strerror(errno));
+    // 	return 1;
+    // }
+    //
+    // struct sockaddr_in serv_addr = { .sin_family = AF_INET ,
+    // 								 .sin_port = htons(4221),
+    // 								 .sin_addr = { htonl(INADDR_ANY) },
+    // 								};
+    //
+    // if (bind(server_fd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) != 0) {
+    // 	printf("Bind failed: %s \n", strerror(errno));
+    // 	return 1;
+    // }
+    //
+    // int connection_backlog = 5;
+    // if (listen(server_fd, connection_backlog) != 0) {
+    // 	printf("Listen failed: %s \n", strerror(errno));
+    // 	return 1;
+    // }
+    //
+    // printf("Waiting for a client to connect...\n");
+    // client_addr_len = sizeof(client_addr);
+    //
+    // accept(server_fd, (struct sockaddr *) &client_addr, &client_addr_len);
+    // printf("Client connected\n");
+    //
+    // close(server_fd);
     server_fd = socket(AF_INET, SOCK_STREAM, 0);
     if (server_fd == -1) {
         printf("Socket creation failed: %s...\n", strerror(errno));
@@ -23,17 +63,15 @@ int main() {
     // Since the tester restarts your program quite often, setting REUSE_PORT
     // ensures that we don't run into 'Address already in use' errors
     int reuse = 1;
-    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEPORT, &reuse, sizeof(reuse)) <
-        0) {
+    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEPORT, &reuse, sizeof(reuse)) < 0) {
         printf("SO_REUSEPORT failed: %s \n", strerror(errno));
         return 1;
     }
-    struct sockaddr_in serv_addr = {
-            .sin_family = AF_INET,
+    struct sockaddr_in serv_addr = { .sin_family = AF_INET ,
             .sin_port = htons(4221),
-            .sin_addr = {htonl(INADDR_ANY)},
+            .sin_addr = { htonl(INADDR_ANY) },
     };
-    if (bind(server_fd, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) != 0) {
+    if (bind(server_fd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) != 0) {
         printf("Bind failed: %s \n", strerror(errno));
         return 1;
     }
@@ -44,11 +82,13 @@ int main() {
     }
     printf("Waiting for a client to connect...\n");
     client_addr_len = sizeof(client_addr);
-    accept(server_fd, (struct sockaddr *)&client_addr, &client_addr_len);
-    int client_fd =
-            accept(server_fd, (struct sockaddr *)&client_addr, &client_addr_len);
+
+    int client_socket_fd = accept(server_fd , (struct sockaddr *)&client_addr,&client_addr_len);
     printf("Client connected\n");
-    write(client_fd, "HTTP/1.1 200 OK\r\n\r\n", 19);
+
+    char response[] = "HTTP/1.1 200 OK \r\n\r\n";
+    send(client_socket_fd , response , sizeof(response) , 0);
+
     close(server_fd);
     return 0;
 }
